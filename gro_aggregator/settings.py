7200d5d8a0a740bb5bf55e849dcedf8785a7dae2
Vince2 <41210985+vitu1@users.noreply.github.com>
2018-07-26 13:41:54 -0400

    Added SECRETKEY changes requested by Tobi. Had to install dotenv

--- gro_aggregator/settings.py
+++ gro_aggregator/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-##from dotenv import load_dotenv ##commented this line out
-##load_dotenv()
+from dotenv import load_dotenv, find_dotenv ##need pip install python-dotenv first
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n8cg5s-r1xnqk(ftj)654$uia#27i6p63+7r76)1cixh6fsj-p' ##os.environ.get('SECRET_KEY') ##using a temporary secret key that can be swapped out with the real one
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
